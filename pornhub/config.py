6641b08c7acddd72af765077792e0323ea5e3e42
sonukumarkushvaha1-hub <sonukumarkushvaha1@gmail.com>
2026-01-14 00:24:18 +0530

    Update config.py

--- pornhub/config.py
+++ pornhub/config.py
@@ -5,7 +5,8 @@ API_HASH: str = "AAG4vKk1WqvfOeZ_nZnJGB3IzamLSWQE4lQ"
 TOKEN: str = "8435607789:AAG4vKk1WqvfOeZ_nZnJGB3IzamLSWQE4lQ"
 
 log_chat: int = -12345678
-sub_chat: str = "channel_username"
+sub_chat: str = "@videoesss_bot"
 sudoers: List[int] = [1757169682, 859229457]
 
 prefixs: List[str] = ["/", "!", ".", "$"]
+
