de918cf459981746a8bbd70228abffd789acda1c
sonukumarkushvaha1-hub <sonukumarkushvaha1@gmail.com>
2026-01-13 23:50:43 +0530

    Update API credentials and add command prefixes

--- pornhub/config.py
+++ pornhub/config.py
@@ -1,10 +1,11 @@
 from typing import List
 
-API_ID: int = 12345
-API_HASH: str = "abcdefg:12345678"
-TOKEN: str = "abcdefg:12345678"
+API_ID: int = 8435607789
+API_HASH: str = "AAG4vKk1WqvfOeZ_nZnJGB3IzamLSWQE4lQ"
+TOKEN: str = "8435607789:AAG4vKk1WqvfOeZ_nZnJGB3IzamLSWQE4lQ"
 
 log_chat: int = -12345678
 sub_chat: str = "channel_username"
 sudoers: List[int] = [1757169682, 859229457]
+
 prefixs: List[str] = ["/", "!", ".", "$"]
